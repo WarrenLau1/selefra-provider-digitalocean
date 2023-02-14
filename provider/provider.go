60a42bb2e4988c3dbd0129fc0783185eacd251c0
WarrenLau1 <124036529+WarrenLau1@users.noreply.github.com>
2023-02-14 15:32:01 +0800

    fix: Update provider config

--- provider/provider.go
+++ provider/provider.go
@@ -87,12 +87,12 @@ func GetProvider() *provider.Provider {
 		},
 		ConfigMeta: provider.ConfigMeta{
 			GetDefaultConfigTemplate: func(ctx context.Context) string {
-				return `##  Optional, Repeated. Add an accounts block for every account you want to assume-role into and fetch data from.
-#accounts:
-#  - token: "<YOUR_API_TOKEN_HERE>" # env DIGITALOCEAN_ACCESS_TOKEN or DIGITALOCEAN_TOKEN
-#    spaces_regions: ["region"]
-#    spaces_access_key: "<YOUR_SPACES_ACCESS_KEY>" # env SPACES_SECRET_ACCESS_KEY
-#    spaces_access_key_id: "<YOUR_SPACES_ACCESS_KEY_ID>" # env SPACES_ACCESS_KEY_ID`
+				return `# token: <Your DigitalOcean Token>
+# spaces_regions:
+#   - ams1
+#   - sfo2
+# spaces_access_key: <Your DigitalOcean Spaces Access Key>
+# paces_access_key_id: <Your DigitalOcean Spaces Access Key Id>`
 			},
 			Validation: func(ctx context.Context, config *viper.Viper) *schema.Diagnostics {
 				var digitaloceanConfig digitalocean_client.Config
